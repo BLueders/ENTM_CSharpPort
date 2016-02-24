5e7822ab53b81795792fae0657861c2f96991890
Benno Lüders <benno.lueders@googlemail.com>
2016-02-24 13:27:59 +0100

    Setup for SeasonTask

---
+++ src/Experiments/SeasonsTask/Container.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTM.Experiments.SeasonsTask
+{
+    struct Season
+    {
+        public int Iteration;
+        public int Type;
+        public Day[] Days;
+    }
+
+    struct Day
+    {
+        public int Iteration;
+        public Food Food;
+    }
+
+    struct Food
+    {
+        public bool IsPoisonous;
+        public int Type;
+    }
+
+    enum SeasonType
+    {
+        Summer = 0,
+        Winter = 1,
+        Spring = 2,
+        Autumn = 3
+    }
+
+    enum FruitType
+    {
+        Apple = 0,
+        Berry = 1,
+        Cherry = 2,
+        Kiwi = 3,
+        Mushroom = 4,
+        Spinach = 5,
+        Pumpkin = 6,
+        Wallnut = 7
+    }
+}
---
+++ src/Experiments/SeasonsTask/SeasonEnvironment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTM.Replay;
+
+namespace ENTM.Experiments.SeasonsTask
+{
+    class SeasonEnvironment : IEnvironment
+    {
+        public bool RecordTimeSteps { get; set; }
+        public EnvironmentTimeStep InitialTimeStep { get; }
+        public EnvironmentTimeStep PreviousTimeStep { get; }
+        public IController Controller { get; set; }
+        public int InputCount { get; }
+        public int OutputCount { get; }
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Restart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double[] InitialObservation { get; }
+        public double[] PerformAction(double[] action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CurrentScore { get; }
+        public double MaxScore { get; }
+        public double NormalizedScore { get; }
+        public bool IsTerminated { get; }
+        public int TotalTimeSteps { get; }
+    }
+}
